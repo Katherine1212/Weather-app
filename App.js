70145a5bd583af6f2339f3eaf69bd5e0603a5125
Katherine1212 <katherinekim971212@gmail.com>
2022-07-29 13:59:35 +0900

    FIX: font 적용//진아 22.07.29

--- App.js
+++ App.js
@@ -1,96 +1,109 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ScrollView, ImageBackground } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 import { MaterialIcons, MaterialCommunityIcons } from '@expo/vector-icons';
 import { LinearGradient } from 'expo-linear-gradient';
 import * as Font from 'expo-font';
 import { useEffect, useState } from 'react';
-import { render } from 'react-dom';
 
-const App = () => {
-  const [load] = Font({
-    Bazzi: require('./assets/fonts/Bazzi.ttf'),
-  })
-  if (!load) {
-    return null;
+export default function App() {
+  const [fontLoad, setFontLoad] = useState(false);
+  useEffect(() => {
+    const Load = async () => {
+      await Font.loadAsync({
+        "Bazzi": require('./assets/fonts/Bazzi.ttf')
+      })
+      setFontLoad(true);
+    }
+    Load();
+  }, []);
+
+  if (fontLoad) {
+    return (
+      <LinearGradient style={{ ...styles.container }} colors={['#7F7FD5', '#86A8E7', '#91EAE4']}>
+        <StatusBar style="dark" />
+        <View style={styles.top_container}>
+          <View style={styles.top_wapper}>
+            <MaterialIcons name="location-on" size={24} color="black" style={{ justifyContent: "center", alignItems: "center" }}>
+              <Text style={styles.location}> 서울시 마포구</Text>
+            </MaterialIcons>
+          </View>
+          <Text style={styles.main_date}>07/14</Text>
+          <View style={styles.main_wrapper}>
+            <MaterialCommunityIcons name="weather-cloudy" size={100} color="black">
+              <View>
+                <Text style={styles.main_temp}>27°C</Text>
+                <Text style={styles.main_weather}>흐림</Text>
+              </View>
+            </MaterialCommunityIcons>
+          </View>
+        </View>
+        <View style={styles.main_desc}>
+          <View style={styles.main_hum}>
+            <MaterialCommunityIcons name="water" size={25} color="black">
+              <Text style={styles.main_humid}>습도 20%</Text>
+            </MaterialCommunityIcons>
+          </View>
+          <View style={styles.main_sun}>
+            <MaterialCommunityIcons name="weather-sunset-up" size={25} color="black">
+              <Text style={styles.main_sunrise}>일출: </Text>
+            </MaterialCommunityIcons>
+            <MaterialCommunityIcons name="weather-sunset-down" size={25} color="black">
+              <Text style={styles.main_sunset}>일몰: </Text>
+            </MaterialCommunityIcons>
+          </View>
+        </View>
+        <ScrollView
+          horizontal
+          showsHorizontalScrollIndicator={false} // ScrollIndicator delete.
+          contentContainerStyle={styles.weekly_container}>
+          <View style={styles.weekly_wrapper}>
+            <Text style={styles.weekly_date}>14일(목)</Text>
+            <MaterialCommunityIcons name="weather-cloudy" size={50} color="black" />
+            <Text style={styles.weekly_temp}>27°C / 24°C</Text>
+
+          </View>
+          <View style={styles.weekly_wrapper}>
+            <Text style={styles.weekly_date}>14일(목)</Text>
+            <MaterialCommunityIcons name="weather-cloudy" size={50} color="black" />
+            <Text style={styles.weekly_temp}>27 / 24</Text>
+
+          </View>
+
+          <View style={styles.weekly_wrapper}>
+            <Text style={styles.weekly_date}>14일(목)</Text>
+            <MaterialCommunityIcons name="weather-cloudy" size={50} color="black" />
+            <Text style={styles.weekly_temp}>27 / 24</Text>
+
+          </View>
+
+          <View style={styles.weekly_wrapper}>
+            <Text style={styles.weekly_date}>14일(목)</Text>
+            <MaterialCommunityIcons name="weather-cloudy" size={24} color="black" />
+            <Text style={styles.weekly_temp}>27 / 24</Text>
+
+          </View>
+
+          <View style={styles.weekly_wrapper}>
+            <Text style={styles.weekly_date}>14일(목)</Text>
+            <MaterialCommunityIcons name="weather-cloudy" size={24} color="black" />
+            <Text style={styles.weekly_temp}>27 / 24</Text>
+
+          </View>
+
+        </ScrollView>
+      </LinearGradient>
+    )
+  } else {
+    return <Text>Loading</Text>
   }
-  return (
-    <LinearGradient style={{ ...styles.container }} colors={['#7F7FD5', '#86A8E7', '#91EAE4']}>
-      <StatusBar style="dark" />
-      <View style={styles.top_container}>
-        <View style={styles.top_wapper}>
-          <MaterialIcons name="location-on" size={24} color="black" style={{ justifyContent: "center", alignItems: "center" }}>
-            <Text style={styles.location}> 서울시 마포구</Text>
-          </MaterialIcons>
-        </View>
-        <Text style={styles.main_date}>07/14</Text>
-        <View style={styles.main_wrapper}>
-          <MaterialCommunityIcons name="weather-cloudy" size={100} color="black">
-            <View>
-              <Text style={styles.main_temp}>27°C</Text>
-              <Text style={styles.main_weather}>흐림</Text>
-            </View>
-          </MaterialCommunityIcons>
-        </View>
-      </View>
-      <View style={styles.main_desc}>
-        <View style={styles.main_hum}>
-          <MaterialCommunityIcons name="water" size={25} color="black">
-            <Text style={styles.main_humid}>습도 20%</Text>
-          </MaterialCommunityIcons>
-        </View>
-        <View style={styles.main_sun}>
-          <MaterialCommunityIcons name="weather-sunset-up" size={25} color="black">
-            <Text style={styles.main_sunrise}>일출: </Text>
-          </MaterialCommunityIcons>
-          <MaterialCommunityIcons name="weather-sunset-down" size={25} color="black">
-            <Text style={styles.main_sunset}>일몰: </Text>
-          </MaterialCommunityIcons>
-        </View>
-      </View>
-      <ScrollView
-        horizontal
-        showsHorizontalScrollIndicator={false} // ScrollIndicator delete.
-        contentContainerStyle={styles.weekly_container}>
-        <View style={styles.weekly_wrapper}>
-          <Text style={styles.weekly_date}>14일(목)</Text>
-          <MaterialCommunityIcons name="weather-cloudy" size={50} color="black" />
-          <Text style={styles.weekly_temp}>27°C / 24°C</Text>
-
-        </View>
-        <View style={styles.weekly_wrapper}>
-          <Text style={styles.weekly_date}>14일(목)</Text>
-          <MaterialCommunityIcons name="weather-cloudy" size={50} color="black" />
-          <Text style={styles.weekly_temp}>27 / 24</Text>
-
-        </View>
-
-        <View style={styles.weekly_wrapper}>
-          <Text style={styles.weekly_date}>14일(목)</Text>
-          <MaterialCommunityIcons name="weather-cloudy" size={50} color="black" />
-          <Text style={styles.weekly_temp}>27 / 24</Text>
-
-        </View>
-
-        <View style={styles.weekly_wrapper}>
-          <Text style={styles.weekly_date}>14일(목)</Text>
-          <MaterialCommunityIcons name="weather-cloudy" size={24} color="black" />
-          <Text style={styles.weekly_temp}>27 / 24</Text>
-
-        </View>
-
-        <View style={styles.weekly_wrapper}>
-          <Text style={styles.weekly_date}>14일(목)</Text>
-          <MaterialCommunityIcons name="weather-cloudy" size={24} color="black" />
-          <Text style={styles.weekly_temp}>27 / 24</Text>
-
-        </View>
-
-      </ScrollView>
-    </LinearGradient>
-  );
 }
 
 const styles = StyleSheet.create({
+  appLoading: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   container: {
     flex: 1
   },
@@ -116,11 +129,14 @@ const styles = StyleSheet.create({
   },
   main_date: {
     fontSize: 50,
-    fontWeight: "500"
+    fontWeight: "500",
+    fontFamily: "Bazzi"
   },
   main_temp: {
     fontSize: 50,
-    fontWeight: "500"
+    fontWeight: "500",
+    fontFamily: "Bazzi",
+    fontFamily: "Bazzi"
   },
   main_icon: {
     fontSize: 50,
@@ -129,12 +145,14 @@ const styles = StyleSheet.create({
   main_weather: {
     fontSize: 30,
     fontWeight: "500",
-    marginLeft: 10
+    marginLeft: 10,
+    fontFamily: "Bazzi"
   },
   main_desc: {
     flex: 0.8,
     flexDirection: "row",
-    alignItems: "center"
+    alignItems: "center",
+    fontFamily: "Bazzi"
   },
   main_hum: {
     flex: 1,
@@ -147,15 +165,18 @@ const styles = StyleSheet.create({
   main_humid: {
     fontSize: 20,
     fontWeight: "500",
-    alignContent: "center"
+    alignContent: "center",
+    fontFamily: "Bazzi"
   },
   main_sunrise: {
     fontSize: 20,
     fontWeight: "500",
+    fontFamily: "Bazzi"
   },
   main_sunset: {
     fontSize: 20,
     fontWeight: "500",
+    fontFamily: "Bazzi"
   },
   weekly_container: {
     maxHeight: 200
@@ -168,15 +189,16 @@ const styles = StyleSheet.create({
   weekly_date: {
     fontSize: 25,
     fontWeight: "500",
+    fontFamily: "Bazzi"
   },
   weekly_weather: {
     fontSize: 25,
-    fontWeight: "500"
+    fontWeight: "500",
+    fontFamily: "Bazzi"
   },
   weekly_temp: {
     fontSize: 25,
-    fontWeight: "500"
+    fontWeight: "500",
+    fontFamily: "Bazzi"
   }
 });
-
-export default App;
